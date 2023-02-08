6d20bc8c1b97bf6b6a40667338513ba6301fd562
Ramon Felix <ramonfsoliveira@gmail.com>
2023-02-08 07:57:34 -0300

    add save

--- javascript.js
+++ javascript.js
@@ -1,13 +1,32 @@
 const form = document.querySelector("#form-habits")
 const nlwSetup = new NLWSetup (form)
+const button = document.querySelector("header button")
 
-const data = {
-    study: ["01-01", "01-02", "01-06"],
-    water: ["01-03"],
-    exercise: ["01-04"],
-    game: ["01-05"],
-    family: ["01-02", "01-04"],
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+    const dayExists = nlwSetup.dayExists (today)
+
+    if (dayExists) {
+        alert('Dia já incluso')
+        return 
+    }
+
+
+    nlwSetup.addDay(today)
+
+
+
+
 }
+
+function save() {
+    localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 
 nlwSetup.setData(data)
 
